9d925636daa1308c61d535e046f934edffd6fc9f
bluszcz <bluszcz@bluszcz.net>
2015-07-07 01:45:07 +0200

    first commit

---
+++ README.rst
@@ -0,0 +1,29 @@
+===============
+django-sb-admin
+===============
+
+Copyright 2015 Rafał Zawadzki
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+Django SB Admin is a Django app which provides a Bootstrap 3 Dashboard theme:
+
+http://startbootstrap.com/template-overviews/sb-admin/
+
+SB Admin is a free to download Bootstrap admin template. This template uses the
+default Bootstrap 3 styles along with a variety of powerful jQuery plugins to 
+create a pwerful framework for creating admin panels, web apps, or back-end dashboards.
+
+SB Admin is licensed under the Apache License 
+https://github.com/IronSummitMedia/startbootstrap-sb-admin/blob/gh-pages/LICENSE
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-sb-admin',
+    version='0.1',
+    packages=['django_sb_admin'],
+    include_package_data=True,
+    license='Apache License version 2.0',  # example license
+    description='',
+    long_description=README,
+    url='https://github.com/bluszcz/django-smb-admin',
+    author='Rafał Zawadzki',
+    author_email='bluszcz@bluszcz.net',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+)
