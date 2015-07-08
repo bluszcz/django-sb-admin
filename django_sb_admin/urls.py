e97e5e973c11e0b87edec411e3c25828223c78b2
bluszcz <bluszcz@bluszcz.net>
2015-07-09 01:18:23 +0200

    Charts, dashboard and tables

--- django_sb_admin/urls.py
+++ django_sb_admin/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     
-    url(r'^$', 'django_sb_admin.views.start'),
+    url(r'^$', 'django_sb_admin.views.start', name='sb_admin_start'),
+    url(r'^dashboard/$', 'django_sb_admin.views.dashboard', name='sb_admin_dashboard'),
+    url(r'^charts/$', 'django_sb_admin.views.charts', name='sb_admin_charts'),
+    url(r'^tables/$', 'django_sb_admin.views.tables', name='sb_admin_tables'),
+    
+    
 
 )
--- django_sb_admin/views.py
+++ django_sb_admin/views.py
@@ -1,7 +1,49 @@
 from django.shortcuts import render
 
-
 def start(request):
     """Start page with a documentation.
     """
-    return render(request, "django_sb_admin/start.html")
+    return render(request, "django_sb_admin/start.html",
+                  {"nav_active":"start"})
+
+def dashboard(request):
+    """Dashboard page.
+    """
+    return render(request, "django_sb_admin/sb_admin_dashboard.html",
+                  {"nav_active":"dashboard"})
+
+def charts(request):
+    """Charts page.
+    """
+    return render(request, "django_sb_admin/sb_admin_charts.html",
+                  {"nav_active":"charts"})
+def tables(request):
+    """Tables page.
+    """
+    return render(request, "django_sb_admin/sb_admin_tables.html",
+                  {"nav_active":"tables"})
+def forms(request):
+    """Forms page.
+    """
+    return render(request, "django_sb_admin/sb_admin_forms.html",
+                  {"nav_active":"forms"})
+def bootstrap_elements(request):
+    """Bootstrap elements page.
+    """
+    return render(request, "django_sb_admin/bootstrap_elements.html",
+                  {"nav_active":"bootstrap_elements"})
+def bootstrap_grid(request):
+    """Bootstrap grid page.
+    """
+    return render(request, "django_sb_admin/bootstrap_grid.html",
+                  {"nav_active":"bootstrap_grid"})
+def dropdown(request):
+    """Dropdown  page.
+    """
+    return render(request, "django_sb_admin/sb_admin_dropdown.html",
+                  {"nav_active":"dropdown"})
+def rtl_dashboard(request):
+    """RTL Dashboard page.
+    """
+    return render(request, "django_sb_admin/sb_admin_rtl_dashboard.html",
+                  {"nav_active":"rtl_dashboard"})
