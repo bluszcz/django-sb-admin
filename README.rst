e6532f2706b90da49e3f9c2e5905646819b6325e
bluszcz <bluszcz@bluszcz.net>
2015-09-26 11:56:36 +0200

    New version

--- README.rst
+++ README.rst
@@ -81,7 +81,7 @@ Extras
 License
 -------
 
-Copyright 2015 Rafał Zawadzki
+Copyright 2015 Rafal Zawadzki
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-sb-admin',
-    version='0.3.3',
+    version='0.3.4',
     packages=['django_sb_admin'],
     include_package_data=True,
     license='Apache License version 2.0',
