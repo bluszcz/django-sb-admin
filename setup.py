6d999a3284e200086c9c2fef0126fb73eae8e744
Rafal Zawadzki <bluszcz@bluszcz.net>
2019-12-11 23:45:12 +0100

    Update the version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-sb-admin',
-    version='5.5.1alpha1',
+    version='5.5.1',
     packages=['django_sb_admin'],
     include_package_data=True,
     license='Apache License version 2.0',
