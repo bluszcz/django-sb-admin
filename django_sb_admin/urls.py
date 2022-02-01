33b029d4b6cb8826bf39cef6895642ba74fa5019
masjohncook <48942450+masjohncook@users.noreply.github.com>
2022-02-01 23:36:54 +0800

    update url to re_path
    update url to re_path which compatibel with Django 4.0+

--- django_sb_admin/urls.py
+++ django_sb_admin/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+# from django.conf.urls import url
 import django_sb_admin.views
 from django.views.generic import TemplateView
-from django.urls import path
+from django.urls import path, re_path
 
 
 
 urlpatterns = [
-    url(r'^$', django_sb_admin.views.start, name='sb_admin_start'),
+    re_path(r'^$', django_sb_admin.views.start, name='sb_admin_start'),
     path('register/', TemplateView.as_view(
         template_name="django_sb_admin/register.html"),
         name='sb_admin_register'
@@ -19,13 +19,13 @@ urlpatterns = [
         template_name="django_sb_admin/sb_admin_404.html"),
         name='sb_admin_404'
     ),
-    url(r'^login/$', django_sb_admin.views.login, name='sb_admin_login'),
-    url(r'^dashboard/$', django_sb_admin.views.dashboard, name='sb_admin_dashboard'),
-    url(r'^charts/$', django_sb_admin.views.charts, name='sb_admin_charts'),
-    url(r'^tables/$', django_sb_admin.views.tables, name='sb_admin_tables'),
-    url(r'^forms/$', django_sb_admin.views.forms, name='sb_admin_forms'),
-    url(r'^bootstrap-elements/$', django_sb_admin.views.bootstrap_elements, name='sb_admin_bootstrap_elements'),
-    url(r'^bootstrap-grid/$', django_sb_admin.views.bootstrap_grid, name='sb_admin_bootstrap_grid'),
-    url(r'^rtl-dashboard/$', django_sb_admin.views.rtl_dashboard, name='sb_admin_rtl_dashboard'),
-    url(r'^blank/$', django_sb_admin.views.blank, name='sb_admin_blank'),
+    re_path(r'^login/$', django_sb_admin.views.login, name='sb_admin_login'),
+    re_path(r'^dashboard/$', django_sb_admin.views.dashboard, name='sb_admin_dashboard'),
+    re_path(r'^charts/$', django_sb_admin.views.charts, name='sb_admin_charts'),
+    re_path(r'^tables/$', django_sb_admin.views.tables, name='sb_admin_tables'),
+    re_path(r'^forms/$', django_sb_admin.views.forms, name='sb_admin_forms'),
+    re_path(r'^bootstrap-elements/$', django_sb_admin.views.bootstrap_elements, name='sb_admin_bootstrap_elements'),
+    re_path(r'^bootstrap-grid/$', django_sb_admin.views.bootstrap_grid, name='sb_admin_bootstrap_grid'),
+    re_path(r'^rtl-dashboard/$', django_sb_admin.views.rtl_dashboard, name='sb_admin_rtl_dashboard'),
+    re_path(r'^blank/$', django_sb_admin.views.blank, name='sb_admin_blank'),
 ]
